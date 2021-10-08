0ee213bfc5e0e60cf815bb61a10dfa75b494f9d0
drogunata <dipeshrogunata_92@hotmail.com>
2021-10-08 23:46:43 +0200

    Criaccao do Modelo e DTO

---
+++ dicr.java.challenge.calculator/src/main/java/com/witsoftware/dicr/java/challenge/calculator/model/Result.java
@@ -0,0 +1,17 @@
+package com.witsoftware.dicr.java.challenge.calculator.model;
+
+import java.math.BigDecimal;
+
+public class Result {
+	
+	private BigDecimal result;
+
+	public BigDecimal getResult() {
+		return result;
+	}
+
+	public void setResult(BigDecimal result) {
+		this.result = result;
+	}
+
+}
---
+++ dicr.java.challenge.calculator/src/main/java/com/witsoftware/dicr/java/challenge/calculator/model/dto/ArithmeticRequest.java
@@ -0,0 +1,46 @@
+package com.witsoftware.dicr.java.challenge.calculator.model.dto;
+
+import java.math.BigDecimal;
+
+import com.witsoftware.dicr.java.challenge.calculator.model.ArithmeticOperationType;
+
+public class ArithmeticRequest {
+
+	private String requestUUID;
+	private BigDecimal numberOne;
+	private BigDecimal numberTwo;
+	private ArithmeticOperationType arithmeticOperationType;
+
+
+	public BigDecimal getNumberOne() {
+		return numberOne;
+	}
+
+	public void setNumberOne(BigDecimal numberOne) {
+		this.numberOne = numberOne;
+	}
+
+	public BigDecimal getNumberTwo() {
+		return numberTwo;
+	}
+
+	public void setNumberTwo(BigDecimal numberTwo) {
+		this.numberTwo = numberTwo;
+	}
+
+	public String getRequestUUID() {
+		return requestUUID;
+	}
+
+	public void setRequestUUID(String requestUUID) {
+		this.requestUUID = requestUUID;
+	}
+
+	public ArithmeticOperationType getArithmeticOperationType() {
+		return arithmeticOperationType;
+	}
+
+	public void setArithmeticOperationType(ArithmeticOperationType arithmeticOperationType) {
+		this.arithmeticOperationType = arithmeticOperationType;
+	}
+}
